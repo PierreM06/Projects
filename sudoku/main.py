585b3d3b4785115e3811124c5657f0cc6b52d2bd
PierreM06 <144691204+PierreM06@users.noreply.github.com>
2024-02-16 17:27:52 +0100

    mogelijkheid voor lastigere sudoku's, zit nog een kleine bug, possible list heeft geen elementen en cell heeft een waarde van 0

--- sudoku/main.py
+++ sudoku/main.py
@@ -1,3 +1,5 @@
+from collections import Counter
+
 class cell():
     def __init__(self, number) -> None:
         self.number = number
@@ -5,27 +7,97 @@ class cell():
         self.possible = [1,2,3,4,5,6,7,8,9]
         pass
 
-    def fill(self):
-        if len(self.possible) == 1:
-            self.sett(self.possible[0])
-
-    def remove(self, waarde):
-        if waarde in self.possible:
+    def remove(self, waarde) -> None:
+        if waarde in self.possible and len(self.possible) > 1:
             self.possible.remove(waarde)
-            self.fill()
+            if len(self.possible) == 1:
+                self.sett(self.possible[0])
 
-    def sett(self, waarde):
+    def sett(self, waarde) -> None:
         self.value = waarde
         if waarde != 0:
             self.possible = []
+            self.update_group()
 
-    def update_group(self):
-        for r in rows[self.number//9]:
-            r.remove(self.value)
-        for c in columns[self.number%9]:
-            c.remove(self.value)
-        for s in squares[self.number//27][int(self.number%9/3)]:
-            s.remove(self.value)
+    def update_group(self) -> None:
+        rows[int(self.number//9)].update_cells(self.value)
+        columns[int(self.number%9)].update_cells(self.value)
+        squares[int((self.number//27*3))+int(self.number%9/3)].update_cells(self.value)
+
+
+class row():
+    def __init__(self, cells: list[cell]) -> None:
+        self.possible = [1,2,3,4,5,6,7,8,9]
+        self.containing = cells
+        pass
+
+    def update_cells(self, waarde) -> None:
+        if waarde in self.possible:
+            self.possible.remove(waarde)
+            for i in self.containing:
+                i.remove(waarde)
+        
+    def group(self) -> None:
+        self.group_possible = []
+        for i in self.containing:
+            for j in i.possible:
+                self.group_possible.append(j)
+        self.voorkomen = Counter(self.group_possible)
+        for key, value in self.voorkomen.items():
+            if value == 1:
+                for i in self.containing:
+                    if key in i.possible:
+                        i.sett(key)
+
+
+class column():
+    def __init__(self, cells: list[cell]) -> None:
+        self.possible = [1,2,3,4,5,6,7,8,9]
+        self.containing = cells
+        pass
+
+    def update_cells(self, waarde) -> None:
+        if waarde in self.possible:
+            self.possible.remove(waarde)
+            for i in self.containing:
+                i.remove(waarde)
+        
+    def group(self) -> None:
+        self.group_possible = []
+        for i in self.containing:
+            for j in i.possible:
+                self.group_possible.append(j)
+        self.voorkomen = Counter(self.group_possible)
+        for key, value in self.voorkomen.items():
+            if value == 1:
+                for i in self.containing:
+                    if key in i.possible:
+                        i.sett(key)
+
+
+class square():
+    def __init__(self, cells: list[cell]) -> None:
+        self.possible = [1,2,3,4,5,6,7,8,9]
+        self.containing = cells
+        pass
+
+    def update_cells(self, waarde) -> None:
+        if waarde in self.possible:
+            self.possible.remove(waarde)
+            for i in self.containing:
+                i.remove(waarde)
+        
+    def group(self) -> None:
+        self.group_possible = []
+        for i in self.containing:
+            for j in i.possible:
+                self.group_possible.append(j)
+        self.voorkomen = Counter(self.group_possible)
+        for key, value in self.voorkomen.items():
+            if value == 1:
+                for i in self.containing:
+                    if key in i.possible:
+                        i.sett(key)
 
 
 def grid():
@@ -34,30 +106,46 @@ def grid():
         if i.value is None:
             getallen.append(0)
         getallen.append(i.value)
+    x=1
     for i in range(9):
         print(f"{getallen[i*9:i*9+3]}|{getallen[i*9+3:i*9+6]}|{getallen[i*9+6:i*9+9]}")
+        if x%3 == 0 and x != 9:
+            print("---------+---------+---------")
+        x += 1
     
 cells = [cell(i) for i in range(9*9)]
 
-rows = [[cells[i+j*9] for i in range(9)] for j in range(9)]
+rows = [row([cells[i+j*9] for i in range(9)]) for j in range(9)]
 
-columns = [[cells[i*9+j] for i in range(9)] for j in range(9)]
+columns = [column([cells[i*9+j] for i in range(9)]) for j in range(9)]
 
-squares = [[[cells[i%3+(i//3*9)+j*3+k*27] for i in range(9)] for j in range(3)] for k in range(3)]
+squares = [square([cells[i%3+(i//3*9)+(j%3)*3+(j//3)*27] for i in range(9)]) for j in range(9)]
 
-eigen_regel = "000020345008000007050094008039400760000309400060075083042951800085730010071248039"
+eigen_regel = "9000500003000000790250900300602090000003085146040630290100964080870003924450872610"
 for i in range(81):
     cells[i].sett(int(eigen_regel[i]))
 
 for i in range(100):
     done = True
+
     for j in cells:
         if j.value != 0:
             j.update_group()
         else:
             done = False
+
+    for r in rows:
+        r.group()
+    for c in columns:
+        c.group()
+    for s in squares:
+        s.group()
+    
     if done:
         print("klaar")
         break
 
 grid()
+
+leeg = [i for i in cells if len(i.possible) == 0 and i.value == 0]
+print(len(leeg))
